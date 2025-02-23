e07a8f63593faeb3c8180d6cefed27230fa3725d
jdunn1998 <dunn2jm@mail.uc.edu>
2025-02-23 14:39:46 -0500

    Initial commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CtoFConverter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double celsius = 0;
+
+        //Loop
+        do {
+            System.out.println("Enter Temperature in Celsius. ";
+
+            if (in.hasNextDouble()) {
+                celsius = in.NextDouble();
+                in.nextline();
+
+            }
+        }
+
+
+
+    }
+}
